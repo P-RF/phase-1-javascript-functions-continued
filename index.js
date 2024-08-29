b69400af7f679b6c0bb7bff04cd7dd8e6773c52f
Priscila Rodriguez Fresco <priscila.rodriguezfresco@gmail.com>
2024-08-29 14:06:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun("bathe my dog!"));
+
+// Define mondayWork function
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Define wrapAdjective function
+function wrapAdjective(style = '*') {
+    return function(adjective) {
+        if (style === '*') {
+            return `You are ${style}${adjective}${style}!`;
+        }   else if (style === '||') {
+            return `You are ${style}${adjective}${style}!`;
+        }
+    }
+}
